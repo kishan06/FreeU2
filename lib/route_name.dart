9281ce7ea4a55f884b3548fdee4675e6c7871d9a
kishan06 <kishan.bhuta1998@gmail.com>
2024-01-17 18:00:17 +0530

    setup 1

---
+++ lib/route_name.dart
@@ -0,0 +1,8 @@
+class RouteName {
+  static const String splashScreen = '/';
+  static const String loginScreen = '/loginScreen';
+  static const String videoDetail = '/videoDetail';
+  static const String videoHomePage = '/videoHomePage';
+  static const String dashboard = '/dashboard';
+  static const String loginintoaccount = '/loginintoaccount';
+}
